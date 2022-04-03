571a9fba57a08cab3f516c8db91ac85940112de7
Esteban <esteban@fwd.dev>
2022-04-03 11:36:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
-# OpenCL
-sudo apt install ocl-icd-opencl-dev 
+# OpenCL, GCC & Build Tools
+sudo apt install ocl-icd-opencl-dev gcc build-essential -y
 
 # Rust
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
-
-# GCC
-sudo apt install gcc -y
 
 # Nano Vanity
 cargo install nano-vanity
